51006c5e5638547a23abe726e7bd28e840e06acd
hrodjr <hector.rodriguez3@gmail.com>
2021-07-11 20:28:13 -0500

    updated exercise. completed all 17 questions.

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,3 +1,5 @@
+# 17 list comprehension exercises in Data Types, Operators, and Variables section exercises.
+
 fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
 numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 17, 19, 23, 256, -8, -4, -2, 5, -9]
 
@@ -5,6 +7,7 @@ numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 17, 19, 23, 256, -8, -4, -2, 5, -
 # Exercise 1 - rewrite the above example code using list comprehension syntax. Make a variable named uppercased_fruits to hold the output of the list comprehension. Output should be ['MANGO', 'KIWI', etc...]
 uppercased_fruits = [fruit.upper() for fruit in fruits]
 print(uppercased_fruits)
+
 
 # Exercise 2 - create a variable named capitalized_fruits and use list comprehension syntax to produce output like ['Mango', 'Kiwi', 'Strawberry', etc...]
 capitalized_fruits = [fruit.capitalize() for fruit in fruits]
@@ -22,29 +25,49 @@ fruits_with_only_two_vowels = [fruit for fruit in fruits if sum (letter in vowel
 print(fruits_with_only_two_vowels)
 
 # Exercise 5 - make a list that contains each fruit with more than 5 characters
+[x for five_char_fruit in fruits if len(x) >= 5]
 
 # Exercise 6 - make a list that contains each fruit with exactly 5 characters
+[x for x in fruits if len(x) == 5]
 
 # Exercise 7 - Make a list that contains fruits that have less than 5 characters
+[x for x in fruits if len(x) <= 5]
 
 # Exercise 8 - Make a list containing the number of characters in each fruit. Output would be [5, 4, 10, etc... ]
+[len(fruit) for fruit in fruits]
 
 # Exercise 9 - Make a variable named fruits_with_letter_a that contains a list of only the fruits that contain the letter "a"
+fruits_with_letter_a = [fruit for fruit in fruits if 'a' in fruit]
+print(fruits_with_letter_a)
 
 # Exercise 10 - Make a variable named even_numbers that holds only the even numbers 
+even_numbers = [even_num for even_num in numbers if even_num % 2 == 0]
+print(even_numbers)
 
 # Exercise 11 - Make a variable named odd_numbers that holds only the odd numbers
+odd_numbers = [odd_num for odd_num in numbers if odd_num % 2 != 0]
+print(odd_numbers)
 
 # Exercise 12 - Make a variable named positive_numbers that holds only the positive numbers
+positive_numbers = [pos_num for pos_num in numbers if pos_num >= 0]
+print(positive_numbers)
 
 # Exercise 13 - Make a variable named negative_numbers that holds only the negative numbers
+negative_numbers = [neg_num for neg_num in numbers if neg_num < 0]
+print(negative_numbers)
 
 # Exercise 14 - use a list comprehension w/ a conditional in order to produce a list of numbers with 2 or more numerals
+[two_or_more for two_or_more in numbers if len(str(two_or_more)) >= 2]
 
 # Exercise 15 - Make a variable named numbers_squared that contains the numbers list with each element squared. Output is [4, 9, 16, etc...]
+numbers_squared = [numsqd ** 2 for numsqd in numbers]
+print(numbers_squared)
 
 # Exercise 16 - Make a variable named odd_negative_numbers that contains only the numbers that are both odd and negative.
+odd_negative_numbers = [odd_neg_num for odd_neg_num in numbers if odd_neg_num < 0 and odd_neg_num % 2 !=0]
+print(odd_negative_numbers)
 
 # Exercise 17 - Make a variable named numbers_plus_5. In it, return a list containing each number plus five. 
-
+numbers_plus_5 = [numplusfive + 5 for numplusfive in numbers]
+print(numbers_plus_5)
 # BONUS Make a variable named "primes" that is a list containing the prime numbers in the numbers list. *Hint* you may want to make or find a helper function that determines if a given number is prime or not.
