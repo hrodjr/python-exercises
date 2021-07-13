8daf1e58d10c9ef76eb7d715fc8d998c307b985e
Hector Rodriguez Jr <hector@Hectors-MacBook-Air.local>
2021-07-13 17:00:24 -0500

    Added import exercises and profile exercise

---
+++ import_exercises.py
@@ -0,0 +1,7 @@
+# Import and test 3 of the functions from your functions exercise file. Import each function in a different way:
+# a. Run an interactive python session and import the module. Call the is_vowel function using the . syntax.
+In [1]: from function_exercises import is_vowel
+In [5]: is_vowel("e")
+Out[5]: True
+
+product("abc", 123)
